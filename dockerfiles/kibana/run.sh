78312f920f53dc17d6c5aec4fb55870ce47170c5
Andrew Peebles <peebles@viblio.com>
2015-09-02 19:11:36 -0700

    bump kibana to 4.1.1 and elasticsearch to 1.4.4

--- dockerfiles/kibana/run.sh
+++ dockerfiles/kibana/run.sh
@@ -4,7 +4,9 @@ KIBANA_USER=${KIBANA_USER-admin}
 KIBANA_PASS=${KIBANA_PASS-password}
 
 htpasswd -b -c /etc/nginx/conf.d/kibana.htpasswd $KIBANA_USER $KIBANA_PASS
-sed -i 's/9200/443/g' /usr/share/nginx/html/config.js
-sed -i 's/http:/https:/g' /usr/share/nginx/html/config.js
 
-nginx -g "daemon off;"
+nginx
+
+cd /deploy/kibana
+./bin/kibana
+
